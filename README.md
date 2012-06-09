e90e54001f954f45fe4e66d55c1bd386f39f0115
chebee7i <chebee7i@gmail.com>
2012-06-09 13:52:24 -0700

    Improved explanations.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ cluster-scripts
 ===============
 
 Some simple cluster scripts.
+
+Presently:
+
+ - submit array jobs in SLURM
--- slurm/arrayjob.sh
+++ slurm/arrayjob.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #
-# Usage: ./arrayjob.sh JOBNAME NUMTASKS WORKERSCRIPT
+# Make sure arrayjob.sh and arrayjob_worker.sh are in your PATH.
+#
+# Usage: arrayjob.sh JOBNAME NUM WORKER_SCRIPT
 #
 #  JOBNAME is the job name and basename for stdout, stderr logfiles.
 #  NUM specifies how many tasks to run.
-#  WORKERSCRIPT should the path to the script to be run.
+#  WORKER_SCRIPT is the path to the script that is to be run.
 #
-# Example:  ./arrayjob.sh myarray 6 ./worker.py
+# Example:  arrayjob.sh myarray 6 ./worker.py
 #
 # This will run
 #
-#   WORKER_SCRIPT=worker.py \
+#   WORKER_SCRIPT=./worker.py \
 #   TASK_ID=X \
 #   sbatch -J myarray.X -o myarray.X.%j.o -e myarray.X.%j.e arrayjob_worker.sh
 #
@@ -18,11 +20,14 @@
 # hostname, working directory, SLURM job id, and task id. After that,
 # it will launch your script as:
 #
-#    time $WORKER_SCRIPT
+#    time ./worker.py
 #
 # So myarray.X.%j.e will contain the output of 'time'.
-# Note: worker.py must be executable (it need not be a Python script either).
-
+#
+# Make sure worker.py uses TASK_ID to customize the work done.
+# Note: The worker script must be executable.
+#       (it need not be a Python script)
+#
 for (( i=0; i<$2; i++ ))
 do
     WORKER_SCRIPT=$3 TASK_ID="$i" sbatch -J $1.$i -o $1.$i.%j.o -e $1.$i.%j.e arrayjob_worker.sh
